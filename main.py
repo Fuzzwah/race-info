b8f9cc4f17348191e57bb9b75fcc2aa63d43af9c
Fuzzwah <rob.crouch@gmail.com>
2015-01-29 07:49:38 -0700

    improved multiclass support, approx points down to 5th, fixed char encoding

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!python3
+#-*- coding: utf-8 -*-
 
 import re
 import irsdk
@@ -6,6 +7,7 @@ from prettytable import PrettyTable
 import constants as ct
 from iRWebStats import iRWebStats
 import config as cfg
+from collections import defaultdict
 
 def median(mylist):
 	sorts = sorted(mylist)
@@ -25,77 +27,113 @@ if cfg.config['password'] == '':
 	cfg.config.write()
 	print("Config saved")
 	
-try:
-	irw.login(cfg.config['username'], cfg.config['password'], quiet=True)
-	web_api = True
-	tab = PrettyTable(['#', 'Name', 'Q Time', 'License', 'iR', 'Races', 'SPos', 'AvgFin', 'AvgInc'])
-	display = ['#', 'Name', 'License', 'iR', 'Races', 'SPos', 'AvgFin', 'AvgInc']
-except:
-	web_api = False
-	tab = PrettyTable(['#', 'Name', 'Q Time', 'License', 'iRating'])
-	display = ['#', 'Name', 'License', 'iR']
-	print("Unable to log into iRacing.com")
-
 if ir.startup():
-
-	qualy_results = dict()
-
-	for res in ir['QualifyResultsInfo']['Results']:
-		qualy_results[res['CarIdx']] = res['FastestTime']
+	if ir['SessionInfo']['Sessions'][1]['SessionType'] == 'Race':
+		try:
+			irw.login(cfg.config['username'], cfg.config['password'], quiet=True)
+			web_api = True
+			tab = PrettyTable(['#', 'Car', 'Name', 'Q Time', 'License', 'iR', 'Races', 'SPos', 'AFin', 'AInc'])
+			display = ['#', 'Car', 'Name', 'License', 'iR', 'Races', 'SPos', 'AFin', 'AInc']
+		except:
+			web_api = False
+			tab = PrettyTable(['#', 'Car', 'Name', 'Q Time', 'License', 'iRating'])
+			display = ['#', 'Car', 'Name', 'License', 'iR']
+			print("Unable to log into iRacing.com")	
 		
-	seriesid = ir['WeekendInfo']['SeriesID']
+		qualy_results = dict()
 
-	ir_total = 0
-	drv_count = 0
-	irs = list()
-	
-	for drv in ir['DriverInfo']['Drivers']:
-		if drv['CarNumberRaw'] > 0:
-			q_time = qualy_results[int(drv['CarIdx'])]
-			if q_time == 0.0:
-				q_time = 999.999
-			name = drv['AbbrevName'].split(', ')
-			name = "%s %s" % (name[1], name[0][:17])
-			row = ([drv['CarNumberRaw'], name, "%.3f" % float(q_time), drv['LicString'], drv['IRating']])
+		for res in ir['QualifyResultsInfo']['Results']:
+			qualy_results[res['CarIdx']] = res['FastestTime']
 			
-			if web_api:
-				drv_last_series = irw.last_series(drv['UserID'])
-				series_stats = False
-				for s in drv_last_series:
-					if s['seriesID'] == seriesid and not series_stats:
-						series_stats = True
-						row.append(s['starts'])
-						row.append(s['position'])
-						row.append(s['avgFinish'])
-						avginc = "%.0f" % float(int(s['incidents']) // int(s['starts']))
-						row.append(avginc)
+		seriesid = ir['WeekendInfo']['SeriesID']
+
+		ir_total = defaultdict(lambda: 0)
+		drv_count = defaultdict(lambda: 0)
+		irs = dict()
+		prev_car = ""
+		mc = False
+		my_car = ""
+		count = 0
+
+		for drv in ir['DriverInfo']['Drivers']:
+			if drv['CarNumberRaw'] > 0:
+				count += 1
+				q_time = qualy_results[int(drv['CarIdx'])]
+				if q_time == 0.0:
+					q_time = 999.999
+				name = drv['AbbrevName'].split(', ')
+				name = "%s %s" % (name[1], name[0][:17])
+				row = ([drv['CarNumberRaw'], drv['CarPath'][:3], name.encode('windows-1252', errors='replace').decode('windows-1252', errors='replace'), "%.3f" % float(q_time), drv['LicString'], drv['IRating']])
 				
-				if not series_stats:
-					row.append('{0: <5}'.format(""))
-					row.append('{0: <5}'.format(""))
-					row.append('{0: <5}'.format(""))
-					row.append('{0: <5}'.format(""))
-			tab.add_row(row)
+				try:
+					irs[drv['CarPath'][:3]] = list()
+				except:
+					print("blah")
+				if irw.custid == drv['UserID']:
+					my_car = drv['CarPath'][:3]
+				
+				if web_api :
+					drv_last_series = irw.last_series(drv['UserID'])
+					series_stats = False
+					for s in drv_last_series:
+						if s['seriesID'] == seriesid and not series_stats:
+							series_stats = True
+							row.append(s['starts'])
+							row.append(s['position'])
+							row.append(s['avgFinish'])
+							avginc = "%.0f" % float(int(s['incidents']) // int(s['starts']))
+							row.append(avginc)
 					
-			ir_total += int(drv['IRating'])
-			irs.append(int(drv['IRating']))
-			drv_count += 1
+					if not series_stats:
+						row.append('{0: <5}'.format(""))
+						row.append('{0: <5}'.format(""))
+						row.append('{0: <5}'.format(""))
+						row.append('{0: <5}'.format(""))
+				tab.add_row(row)
+						
+				ir_total[drv['CarPath'][:3]] += int(drv['IRating'])
+				irs[drv['CarPath'][:3]].append(int(drv['IRating']))
+				drv_count[drv['CarPath'][:3]] += 1
+				if count > 1 and not prev_car == drv['CarPath'][:3]:
+					mc = True
+				prev_car = drv['CarPath'][:3]
+		
+		if my_car == "":
+			my_car = prev_car
+		
+		#print("total ir: %s drv count: %s median ir: %s" % (ir_total[my_car], drv_count[my_car], median(irs[my_car])))
+		sof = float(((int(ir_total[my_car]) / int(drv_count[my_car])) + int(median(irs[my_car]))) / 2)
+		winner_pts = sof / 16
+		pts_diff = (winner_pts / (drv_count[my_car] - 1))
+		
+		print(" ")
+		print("Approx SOF: %.0f" % sof)
+		if drv_count[my_car] > 4:
+			print("Approx PTS: 1st: %.0f | 2nd: %.0f | 3rd: %.0f | 4th: %.0f | 5th: %.0f" % (winner_pts, (winner_pts - pts_diff), (winner_pts - (pts_diff * 2)), (winner_pts - (pts_diff * 3)), (winner_pts - (pts_diff * 4))))
+		elif drv_count[my_car] > 3:
+			print("Approx PTS: 1st: %.0f | 2nd: %.0f | 3rd: %.0f | 4th: %.0f" % (winner_pts, (winner_pts - pts_diff), (winner_pts - (pts_diff * 2)), (winner_pts - (pts_diff * 3))))
+		elif drv_count[my_car] > 2:
+			print("Approx PTS: 1st: %.0f | 2nd: %.0f | 3rd: %.0f" % (winner_pts, (winner_pts - pts_diff), (winner_pts - (pts_diff * 2))))
+		elif drv_count[my_car] > 1:
+			print("Approx PTS: 1st: %.0f | 2nd: %.0f" % (winner_pts, (winner_pts - pts_diff)))
+		else:
+			print("Approx PTS: 1st: %.0f" % (winner_pts))
+			
+		print(" ")
 
-	sof = float(((int(ir_total) / int(drv_count)) + int(median(irs))) / 2)
-	winner_pts_est = sof / 16
-	
-	print(" ")
-	print("Approx SOF: %.0f" % sof)
-	print("Est winner PTS: %.0f" % winner_pts_est)
-	print(" ")
-
-	tab.align['Name'] = 'l'
-	if web_api:
-		tab.align['Races'] = 'r'
-		tab.align['SPos'] = 'r'
-		tab.align['AvgFin'] = 'r'
-		tab.align['AvgInc'] = 'r'
-	table = re.sub("999.999", "       ", tab.get_string(sortby='Q Time', fields=display))
-	print(table)
+		tab.align['Name'] = 'l'
+		if web_api:
+			tab.align['Races'] = 'r'
+			tab.align['SPos'] = 'r'
+			tab.align['AvgFin'] = 'r'
+			tab.align['AvgInc'] = 'r'
+			
+		if not mc:
+			display.pop(1)
+			
+		table = re.sub("999.999", "       ", tab.get_string(sortby='Q Time', fields=display))
+		print(table)
+	else:
+		print("This is not a race session.")
 else:
-	print("iRacing is not running")
+	print("iRacing is not running.")
